be824f125617658ec06518adccc28452b187a9cf
DrHuangMHT <DrHuangMHT@outlook.com>
2022-12-30 22:34:56 +0800

    material icons added

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -17,14 +17,26 @@ const alert = () => {
     <div class="mx-auto">
       <p>v0.0.0</p>
     </div>
-    <button class="" @click="router.push('/')">
-      <img src="../assets/icons/home_black_48dp.svg"/>
+    <button class="tabBtn" @click="router.push('/')">
+      <span class="material-icons m-auto">home</span>
     </button>
-    <button class="" @click="router.push('/overview')">
-      <img src="../assets/icons/dashboard_black_48dp.svg"/>
+    <button class="tabBtn" @click="router.push('/overview')">
+      <span class="material-icons m-auto">dashboard</span>
     </button>
-    <button class="" @click="router.push('/settings')">
-      <img src="../assets/icons/settings_black_48dp.svg"/>
+    <button class="tabBtn" @click="router.push('/settings')">
+      <span class="material-icons m-auto">settings</span>
     </button>
   </div>
 </template>
+<style scoped>
+.tabBtn{
+  margin: 0px;
+  border: none;
+  box-shadow: none;
+  border-radius: 0px;
+  height:4rem;
+}
+.tabBtn:hover{
+  background-color: gainsboro;
+}
+</style>
