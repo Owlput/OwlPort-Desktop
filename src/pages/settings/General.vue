10f2f96a3da141ff4dec1ec42424cdfd29502535
DrHuangMHT <DrHuangMHT@outlook.com>
2022-12-31 21:59:12 +0800

    implement settings for re-enabling popups

--- src/pages/settings/General.vue
+++ src/pages/settings/General.vue
@@ -1,1 +1,19 @@
-<template>General Settings</template>
+<script setup>
+import { ref } from 'vue';
+import DisabledPopups from '../../components/DisabledPopups.vue';
+
+const showNestConnectionSettings = ref(false)
+
+function changeNestAddr(addr) {}
+</script>
+<template>
+  <div class="px-16 py-4">
+    <section class="m-2">
+      <button class="w-full" @click="()=>{showNestConnectionSettings = !showNestConnectionSettings}">
+        <p class="float-left m-2">Nest Connection Settings</p>
+        <span class="material-icons float-right m-2">expand_{{ showNestConnectionSettings?'less':'more' }}</span>
+      </button>
+    </section>
+    <DisabledPopups></DisabledPopups>
+  </div>
+</template>
