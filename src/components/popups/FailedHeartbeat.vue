e4e52a2f6d71a13953825b6cd4a0b23c8e057b84
DrHuangMHT <DrHuangMHT@outlook.com>
2022-12-31 21:58:39 +0800

    FailedHeartbeat styling

--- src/components/popups/FailedHeartbeat.vue
+++ src/components/popups/FailedHeartbeat.vue
@@ -18,16 +18,17 @@ const props = defineProps({
 <template>
   <div class="bg-[#F2C037] rounded-md p-2">
     <p>Heartbeat to Nest failed with</p>
-    <p>{{ reason }}</p>
+    <p class="text-center">{{ reason }}</p>
     <div class="flex justify-evenly m-2">
-      <button @click="props.dismiss">Dismiss</button>
-      <button @click="props.retry">Retry</button>
+      <button @click="props.dismiss"><p>Dismiss</p></button>
+      <button @click="props.retry"><p>Retry</p></button>
     </div>
   </div>
 </template>
 
 <style scoped>
 p {
-  margin: 0px;
+  margin: 0.5rem;
 }
+
 </style>
