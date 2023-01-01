192ac17dae0c71d797f21db4c4398159bdd7aa00
DrHuangMHT <DrHuangMHT@outlook.com>
2023-01-02 00:28:14 +0800

    better openssl interface

--- src-tauri/src/plugins/openssl.rs
+++ src-tauri/src/plugins/openssl.rs
@@ -7,52 +7,44 @@ use tokio::io::AsyncWriteExt;
 
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("openssl_binding")
-        .invoke_handler(tauri::generate_handler![generate_rsa_keypair])
+        .invoke_handler(tauri::generate_handler![generate_rsa_private_key])
         .build()
 }
 
 /// Expecting whitespace separated parameters: `<length> [filename]`
 #[tauri::command]
-async fn generate_rsa_keypair(arg: String) -> Result<String, String> {
-    let mut args = arg.split_ascii_whitespace();
-    let size = match args.next() {
-        Some(v) => match v.parse::<u32>() {
-            Ok(v) => v,
-            Err(e) => return Err(format!("Failed to parse RSA key size: {}", e)),
-        },
-        None => return Err("RSA key size not supplied".into()),
-    };
-    let keypair = match Rsa::generate(size) {
+async fn generate_rsa_private_key(length:u32,filename:Option<String>) -> Result<String, String> {
+    let keypair = match Rsa::generate(length) {
         Ok(keypair) => keypair,
-        Err(e) => return Err(format!("Failed to generate RSA key: {}", e)),
+        Err(e) => return Err(format!("error.plugin.openssl.privateKey.generation: {:?}", e)),
     };
     let pem = keypair.private_key_to_pem().unwrap();
-    let filepath = match args.next() {
+    let filepath = match filename {
         Some(v) => format!("certs/{}.pem", v),
         None => return Ok(String::from_utf8(pem).unwrap()),
     };
     let e = match tokio::fs::File::open(&filepath).await {
-        Ok(_) => return Err("File already exist.".into()),
+        Ok(_) => return Err("error.io.file.open.alreadyExist".into()),
         Err(e) => e,
     };
     if e.kind() == std::io::ErrorKind::NotFound {
         match tokio::fs::File::create(format!("certs")).await {
             Ok(mut handle) => match handle.write_all(&pem).await {
                 Ok(_) => Ok(format!(
-                    "Successfully saved RSA private key to {}",
+                    "ok.plugin.openssl.privateKey.write: {}",
                     filepath
                 )),
                 Err(e) => Err(format!(
-                    "Failed to create file {}, caused by {} |{}",
+                    "error.io.file.write: {}, {} |{}",
                     // always spit out generated key when this fails and display to user when needed.
-                    filepath,
                     e,
+                    filepath,
                     String::from_utf8(pem).unwrap()
                 )),
             },
-            Err(e) => Err(format!("Failed to create file: {}", e)),
+            Err(e) => Err(format!("error.io.file.touch: {}", e)),
         }
     } else {
-        Err(format!("I/O Error: {:#?}", e))
+        Err(format!("error.io.file.unknown: {:#?}", e))
     }
 }
