98493b710c01c17573918fc173c50538f3aeaf01
drHuangMHT <DrHuangMHT@outlook.com>
2025-04-04 15:01:08 +0800

    dial page

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import { createApp } from "vue";
 import router from "./router";
 import "./style.css";
+import "@mdi/font/css/materialdesignicons.css";
 import App from "./App.vue";
-import VueVirtualScroller from 'vue-virtual-scroller'
-import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+import VueVirtualScroller from "vue-virtual-scroller";
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
+import { createVuetify } from "vuetify";
 
-createApp(App).use(router).use(VueVirtualScroller).mount("#app");
+let vuetify = createVuetify({
+  theme: {
+    defaultTheme: "light",
+  },
+});
+
+createApp(App).use(router).use(VueVirtualScroller).use(vuetify).mount("#app");
 
 /*
     Note: Errors from tauri cannot be handled using global error handler 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), vuetify()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
